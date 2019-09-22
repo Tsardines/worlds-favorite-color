0d919b257099be72fcf851ea774324b055dd09d4
unknown <acfranz2@gmail.com>
2019-09-22 13:57:30 -0500

    update favoriteColorCalc

--- databaseFiles/Statistics.java
+++ databaseFiles/Statistics.java
@@ -8,7 +8,8 @@ public class Statistics {
 
     private static int entries;
     private static int[] avgRGB;
-    private static HashMap<String, Integer> amts = new HashMap<>();
+    private static Map<String, Integer> amts = new HashMap<>();
+
 
     public static int[] getAvgRGB(){
         return avgRGB;
@@ -20,8 +21,7 @@ public class Statistics {
 
     public static void evalAvg(int[] newRGB){
         updateAmts(newRGB);
-        int[] avg = new int[3];
-        for(int i = 0; i < avg.length; i++)
+        for(int i = 0; i < avgRGB.length; i++)
             avgRGB[i] = ((avgRGB[i] * entries) + newRGB[i]) / (entries + 1);
         entries++;
     }
@@ -32,6 +32,24 @@ public class Statistics {
             amts.put(rgbStr, amts.get(rgbStr) + 1);
         else
             amts.put(rgbStr, 1);  
+    }
+
+    public static String getFavClr(){
+        String mostPop = null;
+        String equaPop = null;
+        int maxVal = 0;
+
+        for(String str: amts.keySet()){
+            if(amts.get(str) > maxVal){
+                mostPop = str;
+                maxVal = amts.get(str);
+                equaPop = null;
+            }
+            else if(amts.get(str) == maxVal)
+                equaPop = str;
+        }
+        
+        return equaPop == null ? mostPop : mostPop + " " + equaPop;
     }
 
 
@@ -82,6 +100,8 @@ public class Statistics {
         System.out.println(Arrays.toString(avgRGB));
 
         System.out.println(amts);
+
+        System.out.println(getFavClr());
     }
 
 }
