addbde76b0ba05b6faa5618d58b2b1fe561eb47a
eeshaan <me@eeshaan.life>
2019-09-22 12:56:57 -0500

    Add Home.js

---
+++ src/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Container from 'react-bootstrap/Container'
+
+const Home = () => {
+  return(
+    <Container>
+      <Row>
+        <Col>
+          what's your favorite color?
+        </Col>
+      </Row>
+    </Container>
+  )
+}
+
+export default Home;
